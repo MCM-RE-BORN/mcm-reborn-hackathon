bec3bf2ea42cf9a2c7818411017060ad558de8c0
Ahn-taegyu <kundou2016@gmail.com>
2026-08-20 03:00:05 +0900

    tune: replay 3d guidance after 3000ms

--- mcm-reborn/components/screens/analysis-design/MockupViewer.tsx
+++ mcm-reborn/components/screens/analysis-design/MockupViewer.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import styles from "./analysis-design.module.css";
 
 const MODEL_SRC = "/assets/models/reborn-passport-wallet.glb";
-const PROMPT_RESET_DELAY_MS = 1_600;
+const PROMPT_RESET_DELAY_MS = 3_000;
 
 type ModelViewerHandle = HTMLElement & {
   resetInteractionPrompt: () => void;
